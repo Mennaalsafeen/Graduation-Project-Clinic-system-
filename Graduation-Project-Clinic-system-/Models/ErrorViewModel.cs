f754858ecc3dfb8ca6f0397fd1469ea49a37994a
Taima <taimaalsafeen.2020@gmail.com>
2026-04-01 16:19:47 +0300

    Add project files.

---
+++ Graduation-Project-Clinic-system-/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Graduation_Project_Clinic_system_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
